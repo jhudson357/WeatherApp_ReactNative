53108a8542ff0c3e47d04cae6b3eddd74b117cb2
James Hudson <jmh357@comcast.net>
2023-05-18 14:28:36 -0500

    add city screen and IconText component

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { View, StyleSheet } from 'react-native'
 import React from 'react'
 import CurrentWeather from './src/screens/CurrentWeather'
 import UpcomingWeather from './src/screens/UpcomingWeather'
+import City from './src/screens/City'
 
 const App = () => {
   return (
     <View style={styles.container}>
       {/* <CurrentWeather /> */}
-      <UpcomingWeather />
+      {/* <UpcomingWeather /> */}
+      <City />
     </View>
   )
 }
